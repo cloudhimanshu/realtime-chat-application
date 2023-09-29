bd0203b3e833f15b0a25928bba04e11c73b8d81a
Himanshu kaushik <rtxhoney@duck.com>
2023-09-29 20:47:52 +0530

    fixed cors error today and also now its started working working

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,12 +1,21 @@
 const io = require('socket.io')(8000);
 const users = {};
+const express = require('express');
+const cors = require('cors'); // Import the cors package
+
+const app = express();
+
+// Use the cors middleware to enable CORS
+app.use(cors());
+
 io.on('connection', (socket) => {
-    socket.on('new-user-joined', name => {
+    socket.on('new-user-joined', (name) => {
+        console.log("New user", name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
 
     socket.on('send', (message) => {
-        socket.broadcast.emit('receive', { message: message, name: user[socket.id] });
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
     });
 });
