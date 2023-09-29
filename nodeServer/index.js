a798c13ff50e378341f8f82f2fe924d9564f9198
Himanshu kaushik <rtxhoney@duck.com>
2023-09-29 18:40:04 +0530

    new updates

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,8 +1,14 @@
-//Node server which will handel socket io connections
-const io =require('socket.io')(8000)
-const users={};
-io.on('connection',socket=>{
-    socket.on('user-joined',name=>{
+console.log('Server is starting...');
+const io = require('socket.io')(8000);
+const users = {};
+console.log('Server has started.');
+io.on('connection', (socket) => {
+    socket.on('new-user-joined', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
 
-    })
-})
+    socket.on('send', (message) => {
+        socket.broadcast.emit('receive', { message: message, name: user[socket.id] });
+    });
+});
